0cfc247f271ce115458e251ce3340b53965ddb40
Elics21 <paw.shweczow@yandex.ru>
2024-03-12 11:52:36 +0700

    base menu bot

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 import  telebot
-import  webbrowser
+from  telebot import  types
 
 bot = telebot.TeleBot("7017326687:AAEC8O6BS3YrvQbOnOcdFo6IpSLocMKG7DY")
 
-@bot.message_handler(commands=["site"])
-def site(massage):
-    webbrowser.open("https://pypi.org/project/pyTelegramBotAPI/")
+@bot.message_handler(commands=["start"])
+def start(message):
+    markup = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton("Магазин")
+    btn2 = types.KeyboardButton("Информация")
+    markup.row(btn1)
+    markup.row(btn2)
+    bot.send_message(message.chat.id, "Наше меню:", reply_markup=markup)
+    bot.register_next_step_handler(message, on_click)
 
-@bot.message_handler(commands=["start", "hello"])
-def main(massage):
-    if(massage.from_user.id == 860936779):
-        bot.send_message(massage.chat.id, "Вы админ этого бота!")
-    else:
-        bot.send_message(massage.chat.id,
-                         f"К <b>сожалению</b> {massage.from_user.first_name} вы не админ",
-                         parse_mode="html"
-                         )
-@bot.message_handler()
-def info(massage):
-    if (massage.text.lower() == "привет"):
-        bot.send_message(massage.chat.id, f"Првиет {massage.from_user.first_name}!")
-    elif(massage.text.lower() == "id"):
-        bot.reply_to(massage, f"Ваш ID: {massage.from_user.id}")
+def on_click(message):
+    if message.text == "Информация":
+        bot.send_message(message.chat.id, "information")
+    elif message.text == "Магазин":
+        bot.send_message(message.chat.id, "store: ")
+
+@bot.message_handler(commands=["help"])
+def main(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton("Перейти на сайт", url="https://www.youtube.com")
+    markup.row(btn1)
+    btn2 = types.InlineKeyboardButton("Удалиить сообщение", callback_data="delete")
+    btn3 = types.InlineKeyboardButton("Bpvtybnm сообщение", callback_data="edit")
+    markup.row(btn2, btn3)
+    bot.send_message(message.chat.id, "Крутой сайт!", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == "delete":
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+    elif callback.data == "edit":
+        bot.edit_message_text("eding text", callback.message.chat.id, callback.message.message_id)
+
 
 bot.polling(non_stop=True)
